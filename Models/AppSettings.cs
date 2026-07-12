b6522b61e4264772a96be45282958e676853eab9
Lena <henryculverhouse2005@outlook.com>
2026-07-12 23:25:52 +0100

    Added base project files upon commencement of development

---
+++ Models/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Limelight.Models
+{
+    // Add future user preferences here so they can all share one settings file TODO
+    public sealed class AppSettings
+    {
+        public string GameDirectory { get; set; } = string.Empty;
+    }
+}
